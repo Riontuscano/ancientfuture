ca16ff200038b769fb9f96ee050f4b23c8c1dfb3
Rion Tuscano <riontuscano@gmail.com>
2026-03-01 16:45:23 +0530

    cors fixes two

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,13 @@ const allowedOrigins = [
         callback(new Error('Not allowed by CORS'));
       }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
+
+  // Explicitly handle preflight requests
+  app.options('*', cors());
   
 
 app.use(express.json());
